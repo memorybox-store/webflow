fdf495e4a2340006d5a0493cfac41226311d4883
Patiparnne Vongchompue <partyparti@gmail.com>
2023-09-22 16:23:33 +0700

    Init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import UserService from './services/UserService';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello World!");
+  UserService.set();
+  console.log('test');
+});
---
+++ src/services/UserService.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const UserService = {
+  set: async () => {
+    console.log('set');
+    axios.get('/user?ID=12345')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }
+};
+
+export default UserService;
